debf41e716814b84e491dabd2d40138224af5757
sungbum1004 <tlsqkfka96@naver.com>
2021-06-25 13:35:25 +0900

    게시물 수정

--- src/com/sbs/exam/app/App.java
+++ src/com/sbs/exam/app/App.java
@@ -121,7 +121,43 @@ public class App {
 
 				System.out.printf("%d번 게시물을 삭제하였습니다.\n", id);
 
-			} else if (rq.getActionPath().equals("/usr/system/exit")) {
+			} 
+			else if (rq.getActionPath().equals("/usr/article/modify")) {
+				int id = rq.getIntParam("id", 0);
+
+				if (id == 0) {
+					System.out.println("id를 입력해주세요.");
+					continue;
+				}
+
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				System.out.println("제목 : ");
+				String title = sc.nextLine();
+				
+				System.out.println("내용 : ");
+				String body = sc.nextLine();
+				
+				foundArticle.title = title;
+				foundArticle.body = body;
+				foundArticle.updateDate = Util.getNowDateStr();
+				
+				System.out.println(id + "번 게시물을 수정하였습니다.");
+			}
+			
+			else if (rq.getActionPath().equals("/usr/system/exit")) {
 				System.out.println("프로그램을 종료 합니다.");
 				break;
 			}
