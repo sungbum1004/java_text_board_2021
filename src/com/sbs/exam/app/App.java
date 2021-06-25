c4e95b4178a42dc9b6edaa40a97d96dbacc1a3a6
sungbum1004 <tlsqkfka96@naver.com>
2021-06-25 13:11:29 +0900

    최신

---
+++ src/com/sbs/exam/app/App.java
@@ -0,0 +1,133 @@
+package com.sbs.exam.app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.sbs.exam.app.dto.Article;
+import com.sbs.exam.util.Util;
+
+public class App {
+	public static void run() {
+		System.out.println("== 텍스트 게시판 시작 ==");
+
+		Scanner sc = new java.util.Scanner(System.in);
+
+		List<Article> articles = new ArrayList<>();
+		int articlesLastId = 0;
+
+		for (int i = 0; i < 10; i++) {
+			Article article = new Article();
+			article.id = articlesLastId + 1;
+			article.regDate = Util.getNowDateStr();
+			article.updateDate = Util.getNowDateStr();
+			article.title = "제목 " + article.id;
+			article.body = "내용 " + article.id;
+			articles.add(article);
+			articlesLastId++;
+		}
+
+		while (true) {
+			System.out.printf("명령어) ");
+
+			String command = sc.nextLine().trim();
+
+			Rq rq = new Rq(command);
+
+			if (rq.isValid == false) {
+				System.out.printf("명령어가 올바르지 않습니다.\n");
+				continue;
+			}
+
+			if (rq.getActionPath().equals("/usr/article/write")) {
+				System.out.printf("제목 : ");
+				String title = sc.nextLine().trim();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine().trim();
+
+				Article article = new Article();
+				article.id = articlesLastId + 1;
+				article.regDate = Util.getNowDateStr();
+				article.updateDate = Util.getNowDateStr();
+				article.title = title;
+				article.body = body;
+				articles.add(article);
+
+				articlesLastId++;
+
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
+			} else if (rq.getActionPath().equals("/usr/article/list")) {
+				System.out.printf("번호 / 작성날자 / 제목\n");
+
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d / %s / %s\n", article.id, article.regDate, article.title);
+				}
+
+			} else if (rq.getActionPath().equals("/usr/system/exit")) {
+				System.out.println("프로그램을 종료 합니다.");
+				break;
+			} else if (rq.getActionPath().equals("/usr/article/detail")) {
+				int id = rq.getIntParam("id", 0);
+
+				if (id == 0) {
+					System.out.println("id를 입력해주세요.");
+					continue;
+				}
+
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("작성날짜 : %s\n", foundArticle.regDate);
+				System.out.printf("수정날짜 : %s\n", foundArticle.updateDate);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
+			} else if (rq.getActionPath().equals("/usr/article/delete")) {
+				int id = rq.getIntParam("id", 0);
+
+				if (id == 0) {
+					System.out.println("id를 입력해주세요.");
+					continue;
+				}
+
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				articles.remove(foundArticle);
+
+				System.out.printf("%d번 게시물을 삭제하였습니다.\n", id);
+
+			} else if (rq.getActionPath().equals("/usr/system/exit")) {
+				System.out.println("프로그램을 종료 합니다.");
+				break;
+			}
+		}
+
+		System.out.println("== 텍스트 게시판 끝 ==");
+	}
+
+}
---
+++ src/com/sbs/exam/app/Rq.java
@@ -0,0 +1,65 @@
+package com.sbs.exam.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rq {
+	private Map<String, String> params;
+	private String command;
+	private String controllerTypeName;
+	private String controllerName;
+	private String actionMethodName;
+	private String queryString = "";
+	public boolean isValid = true;
+
+	public Rq(String command) {
+		this.command = command;
+		
+		params = new HashMap<>();
+
+		String[] commandBits = command.split("\\?", 2);
+
+		if (commandBits.length == 2) {
+			queryString = commandBits[1];
+
+			String[] queryStringBits = queryString.split("&");
+
+			for (String queryStringBit : queryStringBits) {
+				String[] queryStringBitBits = queryStringBit.split("=", 2);
+				String paramName = queryStringBitBits[0];
+				String paramValue = queryStringBitBits[1];
+				
+				params.put(paramName, paramValue);
+			}
+		}
+
+		commandBits = commandBits[0].split("/", 4);
+
+		if (commandBits.length != 4) {
+			isValid = false;
+			return;
+		}
+
+		controllerTypeName = commandBits[1];
+		controllerName = commandBits[2];
+		actionMethodName = commandBits[3];
+	}
+
+	public Object getActionPath() {
+		return "/" + controllerTypeName + "/" + controllerName + "/" + actionMethodName;
+	}
+
+	public int getIntParam(String paramName, int defaultValue) {
+		if(params.containsKey(paramName) == false) {
+			return defaultValue;
+		}
+		
+		try  {
+			return Integer.parseInt(params.get(paramName));			
+		}
+		catch ( NumberFormatException e ) {
+			return defaultValue;
+		}
+	}
+
+}
